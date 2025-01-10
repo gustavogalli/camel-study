ffb6fecbf081dfdd7269c06bd0a90dc4580d6608
gustavogalli <gustavosgalli@gmail.com>
2025-01-10 19:08:16 -0300

    Copying files

--- camel-a/src/main/java/com/indian/camel_a/routes/b/MyFileRouter.java
+++ camel-a/src/main/java/com/indian/camel_a/routes/b/MyFileRouter.java
@@ -9,6 +9,7 @@ public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:files/input")
+                .log("${body}")
                 .to("file:files/output");
 
     }
